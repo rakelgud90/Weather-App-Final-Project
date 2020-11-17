bdbfed83e226a66af3411c3638e289069c688e06
ritagouveia22 <73960119+ritagouveia22@users.noreply.github.com>
2020-11-17 17:10:30 +0000

    Search engine added

--- src/script.js
+++ src/script.js
@@ -35,8 +35,20 @@ function displayTemperature(response){
   iconElement.setAttribute("alt", response.data.weather[0].description.charAt(0).toUpperCase() + response.data.weather[0].description.slice(1))
 }
 
-let apiKey = "05d59b97163becec12a0f8000856ca3e"
-let city = "Lisbon"
-let apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+function search(city) {
+  let apiKey = "05d59b97163becec12a0f8000856ca3e"
+  let apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
 
-axios.get(apiUrl).then(displayTemperature)
+  axios.get(apiUrl).then(displayTemperature)
+}
+
+function handleSubmit(event) {
+  event.preventDefault()
+  let cityInputElement = document.querySelector("#city-input")
+  search(cityInputElement.value)
+}
+
+search("Lisbon")
+
+let form = document.querySelector("#search-form")
+form.addEventListener("submit", handleSubmit)
