139abd53657310a68f42889eba13904d30bc5e16
ritagouveia22 <73960119+ritagouveia22@users.noreply.github.com>
2020-11-17 17:56:52 +0000

    Added sunset and sunrise real time

--- src/script.js
+++ src/script.js
@@ -17,19 +17,57 @@ function formatDate(timestamp){
   return `${dayOfWeek} ${month} ${day}, ${hours}:${minutes}`
 }
 
+function formatSunrise(timestamp) {
+let date = new Date(timestamp)
+let hours = date.getHours()
+if (hours < 10) {
+  hours = `0${hours}`
+}
+let minutes = date.getMinutes()
+   if (minutes < 10) {
+    minutes = `0${minutes}`
+  }
+  return `${hours}:${minutes}`
+}
+
+function formatSunset(timestamp) {
+let date = new Date(timestamp)
+let hours = date.getHours()
+if (hours < 10) {
+  hours = `0${hours}`
+}
+let minutes = date.getMinutes()
+   if (minutes < 10) {
+    minutes = `0${minutes}`
+  }
+  return `${hours}:${minutes}`
+}
+
 function displayTemperature(response){
   let temperatureElement = document.querySelector("#current-temperature")
   temperatureElement.innerHTML = Math.round(response.data.main.temp)
+  
   let cityElement = document.querySelector("#city")
   cityElement.innerHTML = response.data.name
+  
   let descriptionElement = document.querySelector("#description")
   descriptionElement.innerHTML = response.data.weather[0].description.charAt(0).toUpperCase() + response.data.weather[0].description.slice(1)
+  
   let humidityElement = document.querySelector("#humidity")
   humidityElement.innerHTML = response.data.main.humidity
+  
   let windElement = document.querySelector("#wind")
   windElement.innerHTML = Math.round(response.data.wind.speed)
+  
   let dateElement = document.querySelector("#date")
   dateElement.innerHTML = formatDate(response.data.dt * 1000)
+  
+  let sunriseElement = document.querySelector("#sunrise")
+  let sunsetElement = document.querySelector("#sunset")
+  sunriseElement.innerHTML = formatSunrise(response.data.sys.sunrise * 1000)
+  sunsetElement.innerHTML = formatSunset(response.data.sys.sunset * 1000)
+
+
   let iconElement = document.querySelector("#icon")
   iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`)
   iconElement.setAttribute("alt", response.data.weather[0].description.charAt(0).toUpperCase() + response.data.weather[0].description.slice(1))
