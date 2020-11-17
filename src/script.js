96c9f4c26f827fd04d03b3a1d4dbb7ea3bb78578
ritagouveia22 <73960119+ritagouveia22@users.noreply.github.com>
2020-11-17 20:29:18 +0000

    Added forecast real data

--- src/script.js
+++ src/script.js
@@ -43,6 +43,36 @@ let minutes = date.getMinutes()
   return `${hours}:${minutes}`
 }
 
+function formatForecastDay(timestamp) {
+  let date = new Date(timestamp)
+  let daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+  let dayOfWeek = daysOfWeek[date.getDay()]
+
+  return `${dayOfWeek}`
+}
+
+function displayForecast(response) {
+  let forecastElement = document.querySelector("#weather-forecast")
+  forecastElement.innerHTML = null
+  let forecast = null
+
+  for (let index = 0; index < 6; index++) {
+    forecast = response.data.daily[index];
+    forecastElement.innerHTML += 
+    `<div class="col-2">
+      <p>${formatForecastDay(forecast.dt * 1000)}</p>
+      <img
+      src="http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png"
+      alt="${forecast.weather[0].description}">
+      <p><strong>
+      ${Math.round(forecast.temp.max)} ºC
+      </strong> | 
+      ${Math.round(forecast.temp.min)} ºC</p>
+    </div>`;
+   } 
+  }
+
+
 function displayData(response){
   celsiusTemperature = response.data.main.temp
   let temperatureElement = document.querySelector("#current-temperature")
@@ -68,10 +98,18 @@ function displayData(response){
   sunriseElement.innerHTML = formatSunrise(response.data.sys.sunrise * 1000)
   sunsetElement.innerHTML = formatSunset(response.data.sys.sunset * 1000)
 
-
   let iconElement = document.querySelector("#icon")
   iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`)
   iconElement.setAttribute("alt", response.data.weather[0].description.charAt(0).toUpperCase() + response.data.weather[0].description.slice(1))
+
+  let lat = response.data.coord.lat
+  let lon = response.data.coord.lon
+  let apiKey = "05d59b97163becec12a0f8000856ca3e"
+
+
+  apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`
+  axios.get(apiUrl).then(displayForecast)
+
 }
 
 function search(city) {
